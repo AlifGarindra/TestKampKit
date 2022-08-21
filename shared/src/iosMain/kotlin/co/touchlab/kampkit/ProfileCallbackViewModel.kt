d067cf15f8589222e09a80bdad95242b276f5b1c
fadhlan <fadhlan@weekendinc.com>
2022-08-22 05:50:18 +0700

    add profile swift

---
+++ shared/src/iosMain/kotlin/co/touchlab/kampkit/ProfileCallbackViewModel.kt
@@ -0,0 +1,18 @@
+package co.touchlab.kampkit
+
+import co.touchlab.kampkit.models.CallbackViewModel
+import co.touchlab.kampkit.models.ProfileRepository
+import co.touchlab.kampkit.models.ProfileViewModel
+import co.touchlab.kermit.Logger
+
+@Suppress("Unused")
+class ProfileCallbackViewModel(
+  profileRepository: ProfileRepository,
+  log: Logger
+) : CallbackViewModel() {
+  override val viewModel = ProfileViewModel(profileRepository, log)
+  val profile = viewModel.state.asCallbacks()
+  fun refreshProfile() {
+    viewModel.refreshProfileAuth()
+  }
+}
